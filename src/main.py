f316e7aa4f143334e8ce8e4e4dbec6a993b79a3f
Carlos Garcia <gscarlos.enri@gmail.com>
2025-10-26 00:26:12 -0600

    ejemplo dependencias globales

--- src/main.py
+++ src/main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Query, Request
 from fastapi.responses import PlainTextResponse, Response,  JSONResponse
 from src.routers.movie_router import movie_router
-from fastapi import status
+from fastapi import status, Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from typing import Annotated
 import os
 
-app = FastAPI()
+def dependency1():
+    print("Global Dependency 1")
+    
+def dependency2():
+    print("Global Dependency 2")
+
+app = FastAPI(dependencies=[Depends(dependency1), Depends(dependency2)])
 
 static_path = os.path.join(os.path.dirname(__file__), "static/")
 templates_path = os.path.join(os.path.dirname(__file__), "templates/")
@@ -27,6 +34,24 @@ async def http_error_handler(request: Request, call_next) -> Response | JSONResp
 @app.get("/", tags=["Home"])
 def home(request: Request):
     return templates.TemplateResponse('index.html', {'request': request, 'message': 'Welcome'})
+
+# def common_parameters(start_date: str, end_date: str):
+#     return {"start_date": start_date, "end_date": end_date}
+
+# CommonDep = Annotated[dict, Depends(common_parameters)]
+
+class CommonDep:
+    def __init__(self, start_date: str, end_date: str) -> None:
+        self.start_date = start_date
+        self.end_date = end_date
+
+@app.get('/users')
+def get_users(commons: CommonDep = Depends()):
+    return f"Users created between {commons.start_date} and {commons.end_date}"
+
+@app.get('/customers')
+def get_customers(commons: CommonDep = Depends()):
+    return f"Customers created between {commons.start_date} and {commons.end_date}"
 
 ## Incluir el router de películas
 app.include_router(movie_router, prefix="/movies", tags=["Movies"]) # <-- Agregamos el prefijo y las etiquetas para el router y que solo tengamos que cambiar aqui de ser necesario
