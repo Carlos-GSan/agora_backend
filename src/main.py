63e264fce7e9704edc9bdb1a9c8babce5a0c82d2
Carlos Garcia <gscarlos.enri@gmail.com>
2025-10-25 23:48:25 -0600

    integrando Jinja2

--- src/main.py
+++ src/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse, Response,  JSONResponse
 from src.routers.movie_router import movie_router
 from fastapi import status
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
+
+static_path = os.path.join(os.path.dirname(__file__), "static/")
+templates_path = os.path.join(os.path.dirname(__file__), "templates/")
+
+app.mount("/static", StaticFiles(directory=static_path), "static")
+templates = Jinja2Templates(directory=templates_path)
 
 @app.middleware('http')
 async def http_error_handler(request: Request, call_next) -> Response | JSONResponse:
@@ -16,8 +25,8 @@ async def http_error_handler(request: Request, call_next) -> Response | JSONResp
     
 
 @app.get("/", tags=["Home"])
-def home():
-    return PlainTextResponse(content="Home", status_code=200)
+def home(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request, 'message': 'Welcome'})
 
 ## Incluir el router de películas
 app.include_router(movie_router, prefix="/movies", tags=["Movies"]) # <-- Agregamos el prefijo y las etiquetas para el router y que solo tengamos que cambiar aqui de ser necesario
